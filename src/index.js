f8a9839940058f2b2cd63fee846e5ea7ee0c1b99
Daniel Cambinda <danielcambinda@gmail.com>
2022-03-29 19:35:20 +0100

    getting started with socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const path = require("path");
+const path = require("path")
+const http = require('http')
 const express = require("express")
+const socketio = require('socket.io') // or const { Server } = require("socket.io");
 
 const app = express()
+const server = http.createServer(app) //express creates this in the background but we cant access the server variable so we have to manually create it to pass it to socket.io
+const io = socketio(server) //or new Server(server); to create a socket.io server 
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public")
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', ()=>{
+    console.log('new websocket connection')
+})
+
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
